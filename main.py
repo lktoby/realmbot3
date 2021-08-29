80923e9375f943a6e3b61047b848a92be51a750a
6days9weeks <no-reply@6days9weeks.wtf>
2021-08-29 13:14:04 +0900

    add rich

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import logging
 import os
 import sys
 import traceback
 
 import discord
+import rich
 from discord.ext import commands
 from dotenv import load_dotenv
 
-intents = discord.Intents.all()
-intents.members = True
+log = logging.getLogger(__name__)
 
 game = discord.Activity(name="i'm back from the dead", type=discord.ActivityType.playing)
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("r!"), intents=intents, activity=game)
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("r!"), intents=discord.Intents.all(), activity=game
+)
 bot.remove_command("help")
 
 
@@ -20,9 +23,20 @@ TOKEN = os.getenv("TOKEN")
 
 @bot.event
 async def on_ready():
-    print("---------------------")
-    print(f"logged in as {bot.user.name}")
-    print("---------------------")
+    startup_msg = [
+        "---------------------",
+        f"logged in as {bot.user.name}",
+        f"i am owned by {str(await bot.fetch_user(bot.owner_id))} [{bot.owner_id}]",
+        f"Prefix: r!",
+        "Connected to:",
+        f"{len(bot.guilds)} servers",
+        f"Invite URL: https://discord.com/oauth2/authorize?client_id={bot.user.id}&scope=bot+applications.commands&permissions=0",
+        "---------------------",
+    ]
+    WIDTH = len(max(startup_msg, key=len))
+    startup_msg = [s.center(WIDTH) for s in startup_msg]
+    [startup_msg.insert(index, "-" * WIDTH) for index in (0, 2, len(startup_msg) + 2)]
+    rich.print("\n".join(startup_msg))
 
 
 initial_extensions = ["cogs.info", "cogs.portals", "cogs.noms", "cogs.fun"]
@@ -32,7 +46,7 @@ if __name__ == "__main__":
         try:
             bot.load_extension(extension)
         except Exception as e:
-            print(f"Failed to load extension {extension}.", file=sys.stderr)
+            log.error(f"Failed to load extension {extension}.", file=sys.stderr)
             traceback.print_exc()
 
 bot.run(TOKEN)
